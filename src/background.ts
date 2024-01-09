a6f3f84ff409d18b845434c4be795ba2702c21ea
Arda Akcagöz <a.akcgz002@hotmail.com>
2024-01-09 09:26:12 +0300

    Fixed IDs

--- src/background.ts
+++ src/background.ts
@@ -2,15 +2,21 @@
 
 import type { DownloadURLObj, MsgResponse } from "./shared";
 
+let counter = 0;
+
 chrome.downloads.onDeterminingFilename.addListener((item) => {
     chrome.downloads.erase({ id: item.id });
     const urlObj: DownloadURLObj = {
-        id: 0,
+        id: counter,
         url: item.finalUrl, 
         filesize: item.fileSize, 
         title: item.filename, 
     };
-    fetch("http://localhost:4000", { method: "POST", body: JSON.stringify(urlObj) }).then((res) => console.log(res));
+    fetch("http://localhost:4000", { method: "POST", body: JSON.stringify(urlObj) })
+        .then((res) => {
+            console.log(res);
+        });
+    counter++;
     chrome.runtime.sendMessage<DownloadURLObj, MsgResponse>(urlObj, (res) => {
         res.status == "error"
             ? console.log("Error occurred while sending.")
